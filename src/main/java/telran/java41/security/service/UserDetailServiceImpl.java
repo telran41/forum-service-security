c11425e2f4cf3720de959c26926df13c54735c64
EddFish <edwardlfish@gmail.com>
2022-03-29 22:44:52 +0300

    First commit

---
+++ src/main/java/telran/java41/security/service/UserDetailServiceImpl.java
@@ -0,0 +1,32 @@
+package telran.java41.security.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import telran.java41.accounting.dao.UserAccountRepository;
+import telran.java41.accounting.model.UserAccount;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+	
+	UserAccountRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserAccount userAccount = repository.findById(username)
+				.orElseThrow(() -> new UsernameNotFoundException(username));		
+		String[] roles = userAccount.getRoles().stream()
+							.map(r -> "ROLE_" + r.toUpperCase())
+							.toArray(String[]::new);
+		return new User(username, userAccount.getPassword(), 
+				AuthorityUtils.createAuthorityList(roles));
+	}
+
+	
+}
